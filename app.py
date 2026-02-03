703361fb5af8fc3bae7eea97855746e1fa46888b
harikrishnahk18 <harikrishnaspider45@gmail.com>
2026-02-04 01:42:07 +0530

    front page

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 import streamlit as st
+import base64
 
 st.set_page_config(layout="wide")
+def get_base64(img):
+    with open(img, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+    
+logo= get_base64("Spider_logo.png")
+
 
 st.markdown("""
 <style>
 
-/* Premium dark hardware background */
+
 div[data-testid="stAppViewContainer"] {
-    background-color: #0B1320 !important;
+    background-color: #0B1F3A !important;
 }
 
-/* Futuristic font */
-@import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap');
 
-/* Center glowing header */
+<link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@700&display=swap" rel="stylesheet">
+
+
+.logo{
+            {
+            position: fixed;
+            top: 28px;
+            left: 35px;
+            width: 110px;
+            }
+            }
 .cool-header {
     position: fixed;
     top: 50%;
@@ -21,21 +36,24 @@ div[data-testid="stAppViewContainer"] {
     transform: translate(-50%, -50%);
     z-index: 9999;
 
-    font-family: 'Orbitron', sans-serif;
+    font-family: 'Rajdhani', sans-serif !important;
     font-size: 90px;
     letter-spacing: 4px;
     text-align: center;
 
-    color: #00F5FF;
+    color: #EAF2F;
 
     text-shadow:
-        0 0 10px #00F5FF,
-        0 0 20px #00F5FF,
-        0 0 40px #00F5FF,
-        0 0 80px #00F5FF;
+        0 0 10px #EAF2F,
+        0 0 20px #EAF2F,
+        0 0 40px #EAF2F,
+        0 0 80px #EAF2F;
 }
 
 </style>
 """, unsafe_allow_html=True)
 
-st.markdown('<div class="cool-header">SPIDER HARDWARE WORKSHOP</div>', unsafe_allow_html=True)
+st.markdown('<div class="cool-header">WELCOME TO SPIDER WORKSHOP</div>', unsafe_allow_html=True)
+
+
+
---
+++ appmain.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import base64
+
+st.set_page_config(layout="wide")
+col1, col2, col3 = st.columns([8,1,1])
+
+with col2:
+    st.button("ABOUT US")
+
+with col3:
+    st.button("CONTACT US")
+
+def get_base64(img):
+    with open(img, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+    
+logo= get_base64("Spider_logo.png")
+
+
+st.markdown("""
+<style id = "newstyle">
+
+
+
+
+div[data-testid="stAppViewContainer"] {
+    background-color: #000000 !important;
+}
+
+
+
+.logo{
+            {
+            position: fixed;
+            top: 28px;
+            left: 35px;
+            width: 110px;
+            }
+            }
+.cool-header {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 9999;
+
+    font-family: 'Segoe UI', consolas, monospace !important;
+    font-size: 90px;
+    letter-spacing: 4px;
+    text-align: center;
+
+    color: #4DA3FF;
+
+   
+}
+.bottom-btn {
+    position: fixed !important;
+    left: 50% !important;
+    bottom: 30px !important;
+    transform: translateX(-50%) !important;
+    z-index: 99999 !important;
+
+    padding: 14px 38px;
+    border-radius: 40px;
+
+    font-family: "Segoe UI", Consolas, monospace;
+    font-size: 20px;
+    letter-spacing: 2px;
+
+    color: #000000;
+    background-color: #EAF2FF;
+
+    border: none;
+    cursor: pointer;
+
+    transition: all 0.3s ease;
+}
+
+.bottom-btn:hover {
+    background-color: #7FDBFF;
+    transform: translateX(-50%) scale(1.05);
+}
+
+
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<div class="cool-header">WELCOME TO SPIDER WORKSHOP</div>', unsafe_allow_html=True)
+st.markdown('<button class="bottom-btn">ENTER THE ARENA ---></button>', unsafe_allow_html=True)
+
+
